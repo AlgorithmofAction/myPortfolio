658bbb850ded0d35bde6439b013a7b4dd3ceae83
Kirill <algorithmofaction@gmail.com>
2025-08-30 01:44:49 +0300

    Updates

---
+++ assets/ContactsView-B4BBb0AT.js
@@ -0,0 +1,1 @@
+import{C as a}from"./Contacts-01zCUjn7.js";import{c as e,a as s,b as o,o as c}from"./index-B-NPMhVO.js";const p={__name:"ContactsView",setup(n){return(r,t)=>(c(),e("main",null,[t[0]||(t[0]=s("h1",{class:"contacts-page__title"},"Связаться со мной:",-1)),o(a,{class:"contacts-page mt"})]))}};export{p as default};
